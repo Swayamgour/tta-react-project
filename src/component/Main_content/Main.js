6f0f77232ffe292338c1ede456cbf432c0b3d410
Swayamgour <119596165+Swayamgour@users.noreply.github.com>
2023-09-22 17:43:46 +0530

    4 files modified

--- src/component/Main_content/Main.js
+++ src/component/Main_content/Main.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import './Main.css';
+import "./Main.css";
+
 
 const Main = () => {
   return (
     <div className="centered-container">
       <div className="content">
-        <h1>Centered Content</h1>
+        <h1 className="up-down-animation">Centered Content</h1>
         <p>This content is centered horizontally and vertically.</p>
       </div>
     </div>
   );
-};
+}
 
 export default Main;
+
